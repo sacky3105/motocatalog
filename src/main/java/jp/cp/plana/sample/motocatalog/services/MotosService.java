ed8eb808cdbbf96bb20b1b735f99e6f27b2440e7
sacky3105 <kgglee.114.baritone@gmail.com>
2023-05-05 22:59:00 +0900

    テストコードを作成したが、一部エラー発生中。

--- src/main/java/jp/cp/plana/sample/motocatalog/services/MotosService.java
+++ src/main/java/jp/cp/plana/sample/motocatalog/services/MotosService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import jp.cp.plana.sample.motocatalog.beans.Brand;
 import jp.cp.plana.sample.motocatalog.beans.Motorcycle;
+import jp.cp.plana.sample.motocatalog.beans.SearchCondition;
 import jp.cp.plana.sample.motocatalog.mappers.BrandMapper;
 import jp.cp.plana.sample.motocatalog.mappers.MotorcycleMapper;
 
@@ -19,12 +20,20 @@ public class MotosService {
     @Autowired
     BrandMapper brandMapper;
 
-    public List<Motorcycle> getMotos() {
-        return motorcycleMapper.selectAll();
+    public List<Motorcycle> getMotos(SearchCondition condition) {
+        return motorcycleMapper.selectByCondition(condition);
+    }
+
+    public Motorcycle getMotos(int motoNo) {
+        return motorcycleMapper.selectByPK(motoNo);
     }
 
     public List<Brand> getBrands(){
         return brandMapper.selectAll();
+    }
+
+    public int save(Motorcycle moto) {
+        return motorcycleMapper.update(moto);
     }
     
 }
